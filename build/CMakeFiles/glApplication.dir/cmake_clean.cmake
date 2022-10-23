02438ec01942e00ec21ae766fa5d71b5b4ca736b
sunlulu.tomato <sunlulu.tomato@bytedance.com>
2022-10-24 00:01:24 +0800

    first commit

---
+++ build/CMakeFiles/glApplication.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glApplication.dir/main.cpp.o"
+  "CMakeFiles/glApplication.dir/main.cpp.o.d"
+  "glApplication"
+  "glApplication.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * @FilePath: main.cpp
+ * @Author: sunlulu.tomato@bytedance.com
+ * @Date: 2022-10-23 23:30:12
+ * 
+ * This file is part of LiveCore project. 
+ * Copyright (c) 2022 by LiveCore Team of ByteDance, All Rights Reserved.
+ */
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+using namespace std;
+
+void init(GLFWwindow* window) {
+
+}
+
+
+
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
